76eea08f6e3603c2d6a4f7c16cf609913c970499
nikita <nikii1405abraham@gmail.com>
2023-07-31 15:21:59 -0700

    Create all my plants

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+#plant list
+plants = [
+   {'name':'Neem',
+          'description':'fast-growing tree of the mahogany family (Meliaceae), valued as a medicinal plant, as a source of organic pesticides, and for its timber.',
+          'size': 30,
+          'category':'Tree'},
+          {'name':'Rose',
+          'description':'A rose is either a woody perennial flowering plant of the genus Rosa in the family Rosaceae or the flower it bears.',
+          'size': 1.25,
+          'category':'Shrubs'},
+          ]
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def plants_index(request):
+    return render(request, 'plants/index.html', {
+    'plants': plants
+  })
